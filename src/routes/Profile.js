4a37f59188326947dee502d9fac9f3f0e6771c95
SuJin <1127sujink@naver.com>
2022-07-13 15:32:43 +0900

    07-2 완료

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,31 +1,55 @@
+import { eventWrapper } from "@testing-library/user-event/dist/utils";
 import { authService, dbService } from "fbase";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 
 const Profile = ({ userObj }) => {
     const history = useHistory();
+    const [newDisplayName, seteNewDisplayName] = useState(userObj.newDisplayName);
     
     const onLogOutClick = () => {
         authService.signOut();
         history.push("/");
     };
 
-    const getMyNweets = async () => {
-        const nweets = await dbService
-            .collection("nweets")
-            .where("creatorId", "==", userObj.uid)     // where함수로 creatorId 필드에서 userObj.uid와 같은 값을 찾기 위한 표현
-            // .orderBy("createdAt")           // 오름차순 정리
-            .get(); // 앞에 3개 collection, where, orderBy 실행하는 함수
+    // const getMyNweets = async () => {
+    //     const nweets = await dbService
+    //         .collection("nweets")
+    //         .where("creatorId", "==", userObj.uid)     // where함수로 creatorId 필드에서 userObj.uid와 같은 값을 찾기 위한 표현
+    //         // .orderBy("createdAt")           // 오름차순 정리
+    //         .get(); // 앞에 3개 collection, where, orderBy 실행하는 함수
 
-        console.log(nweets.docs.map((doc) => doc.data()));
-    };
+    //     console.log(nweets.docs.map((doc) => doc.data()));
+    // };
 
-    useEffect(() => {
-        getMyNweets();
-    }, []);
+    // useEffect(() => {
+    //     getMyNweets();
+    // }, []);
+
+    const onChange = (event) => {
+        const {
+            target: {value},
+        } = event;
+        seteNewDisplayName(value);
+    }
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        if (userObj.newDisplayName !== newDisplayName) {
+            await userObj.updateProfile({ displayName: newDisplayName});
+        }
+    }
 
     return (
         <>
+            <form onSubmit={onSubmit}>
+                <input 
+                    onChange={onChange}
+                    type="text" placeholder="Display name" 
+                    value={newDisplayName}
+                />
+                <input type="submit" value="Update Profile" />
+            </form>
             <button onClick={onLogOutClick}>Log out</button>
         </>
     );
